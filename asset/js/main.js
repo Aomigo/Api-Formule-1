82c27bbbe88c25db0a14307625819a12c0adb957
Drake <150128459+VialsShiny@users.noreply.github.com>
2024-04-11 20:01:21 +0200

    V.0.1 | Menu
    ✔API Infos Fetch !
    ✔Beaty Css, Kawaiiiiiiii ♥
    👺Responsive not Finish

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,15 +1,18 @@
 function addResult(e) {
-  const race = e.MRData.RaceTable.Races[0];
-  const winner = race.Results[0];
-  const otherDrivers = race.Results.slice(1);
+  let race = e.MRData.RaceTable.Races[0];
+  let winner = race.Results[0];
+  let otherDrivers = race.Results.slice(1);
+  console.log(otherDrivers);
 
-  const winnerNameElement = document.getElementById('winner-name');
-  const winnerImageElement = document.getElementById('winner-image');
-  const winnerTimeElement = document.getElementById('winner-time');
-  const winnerPositionElement = document.getElementById('winner-position');
-  const otherDriversContainer = document.getElementById('other-drivers');
+  const winnerFirstName = document.querySelector('#winner-first-name');
+  const winnerLastName = document.querySelector('#winner-last-name');
+  const winnerImageElement = document.querySelector('#winner-image');
+  const winnerTimeElement = document.querySelector('#winner-time');
+  const winnerPositionElement = document.querySelector('#winner-position');
+  const otherDriversContainer = document.querySelector('#other-drivers');
 
-  winnerNameElement.textContent = `${winner.Driver.givenName} ${winner.Driver.familyName}`;
+  winnerFirstName.textContent = `${winner.Driver.givenName}`;
+  winnerLastName.textContent = `${winner.Driver.familyName}`;
   winnerImageElement.src = `./asset/img/C/FichePilote/${(winner.Driver.familyName).toLowerCase()}.png`;
   winnerTimeElement.textContent = winner.Time.time;
   winnerPositionElement.textContent = winner.position;
@@ -17,25 +20,33 @@ function addResult(e) {
   otherDrivers.forEach(driver => {
     const driverElement = document.createElement('div');
     driverElement.classList.add('drivers-wrap');
+    driverElement.style.backgroundImage = `url(./asset/img/C/F1Flag/${(driver.Driver.nationality.toLowerCase())}.png)`;
 
     const strongElement = document.createElement('strong');
     strongElement.textContent = driver.position;
 
     const imgElement = document.createElement('img');
-    imgElement.src = `./asset/img/C/FichePilote/${driver.Driver.driverId}.png`;
+    imgElement.src = `./asset/img/C/FichePilote/${driver.Driver.familyName}.png`; 
     imgElement.alt = `${driver.position} place`;
 
     const pElement = document.createElement('p');
     pElement.classList.add('time');
-    pElement.textContent = `${driver.Time.time}`;
+
+    // Vérifier si le temps est disponible
+    if (driver.Time && driver.Time.time) {
+      pElement.textContent = `${driver.Time.time}`;
+    } else {
+      pElement.textContent = `${driver.status}`
+      pElement.style.fontWeight = "bold"
+    }
 
     driverElement.appendChild(strongElement);
     driverElement.appendChild(imgElement);
     driverElement.appendChild(pElement);
 
     otherDriversContainer.appendChild(driverElement);
-    console.log(driver.Driver.driverId);
   });
+
 }
 
 document.addEventListener("DOMContentLoaded", () => {
